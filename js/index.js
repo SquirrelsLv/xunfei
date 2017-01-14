f01815c5cd2f935063ad39eeec62792fe36c499e
SquirrelsLv <171627940@qq.com>
2017-01-14 15:45:27 +0800

    添加nav中的一些js功能及css样式

--- js/index.js
+++ js/index.js
@@ -2,6 +2,15 @@
  * Created by Administrator on 2017/1/12 0012.
  */
 $(function () {
+
+    // nav,与 download切换显示;
+    function downl(index) {
+        if (index == '0'){
+            $('#nav .download').addClass('curo').siblings('.nav').removeClass('curo');
+        }else {
+            $('.nav').addClass('curo').siblings('.download').removeClass('curo');
+        }
+    }
     var index = 0;
     var timer = null;
     //侧边栏鼠标移入移出效果
@@ -14,6 +23,7 @@ $(function () {
     //侧边栏鼠标点击效果
     $('.side li').click(function () {
         index = $(this).index();
+        downl(index);
         $('.side li').eq(index).addClass('cur').siblings().removeClass('cur');
         $('section').eq(index).show().siblings('section').hide();
     });
@@ -25,11 +35,20 @@ $(function () {
             if(index<0 || index>6){
                 index = 0;
             }
-            if (index == 0){
-                
-            }
+            downl(index);
             $('.side li').eq(index).addClass('cur').siblings().removeClass('cur');
             $('section').eq(index).show().siblings('section').hide();
         },200);
     });
+
+    //下载按钮鼠标移入显示对应的二维码
+    $('#nav .download a').hover(function () {
+        $('#nav .download img').eq($(this).index()).toggle();
+    });
+
+
+
+
+    
+    
 })
