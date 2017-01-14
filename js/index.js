e05d4c227c9b675eb9a897b24a5a52cd68aeabd1
SquirrelsLv <171627940@qq.com>
2017-01-14 14:15:33 +0800

    side侧边栏html,css和js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
 /**
  * Created by Administrator on 2017/1/12 0012.
  */
+$(function () {
+    var index = 0;
+    var timer = null;
+    //侧边栏鼠标移入移出效果
+    $('.side li').hover(function () {
+        $(this).addClass('cur').siblings().removeClass('cur');
+    },function () {
+        $('.side li').eq(index).addClass('cur').siblings().removeClass('cur');
+    });
+
+    //侧边栏鼠标点击效果
+    $('.side li').click(function () {
+        index = $(this).index();
+        $('.side li').eq(index).addClass('cur').siblings().removeClass('cur');
+        $('section').eq(index).show().siblings('section').hide();
+    });
+    //滑动滚轮
+    $(window).mousewheel(function (ev,d) {
+        clearTimeout(timer);
+        timer=setTimeout(function () {
+            index -= d;
+            if(index<0 || index>6){
+                index = 0;
+            }
+            if (index == 0){
+                
+            }
+            $('.side li').eq(index).addClass('cur').siblings().removeClass('cur');
+            $('section').eq(index).show().siblings('section').hide();
+        },200);
+    });
+})
